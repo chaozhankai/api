c42806b52c15ea84291012c1f8d2291f0271e9b9
zhuweiyou <i@zhuweiyou.com>
2019-03-29 15:33:50 +0800

    修复短链接 403 问题 (#28)

--- src/main/java/com/mtdhb/api/util/Connections.java
+++ src/main/java/com/mtdhb/api/util/Connections.java
@@ -67,6 +67,7 @@ public class Connections {
     private static URLConnection openConnection(String spec) throws IOException {
         URL url = new URL(spec);
         URLConnection connection = url.openConnection();
+        connection.addRequestProperty("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1");
         connection.setConnectTimeout(DEFAULT_TIME_OUT);
         connection.setReadTimeout(DEFAULT_TIME_OUT);
         return connection;
